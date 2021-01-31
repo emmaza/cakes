58ec7427dc23d00ae6f7a989c2ece9bd6cb969f6
Jing Yang <jyang.ct@gmail.com>
2021-01-30 22:33:19 -0800

    change a few pics

--- src/About/About.js
+++ src/About/About.js
@@ -5,8 +5,8 @@ function About() {
     return (
         <div className="about">
             <h4>About:</h4>
-            <p> All products made with finest ingredients. </p>
-            <p> We are located in East Bay Walnut Creek. </p>
+            <p> All products are made with finest ingredients. </p>
+            <p> We are located in East Bay Walnut Creek (California). </p>
             <p> All cakes and bread can be customized. Please contact the chef for details.  </p>
             <p> Delivery available with extra charge based on distance. </p>
         </div>
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 import img1 from '../images/IMG_8647.JPG';
+import bdaycake from '../images/bdaycake.jpg';
+import Cupcakes from '../images/cupcakes.jpg';
 import './Products.css';
 
 function Products() {
     return (
         <div className="products">
-            <ProductCard image={img1} title="Birthday Cakes" description="homemade with love" />
-            <ProductCard image={img1} title="Birthday Cakes" description="homemade with love" />
-            <ProductCard image={img1} title="Birthday Cakes" description="homemade with love" />
-            <ProductCard image={img1} title="Birthday Cakes" description="homemade with love" />
+            <ProductCard image={bdaycake} title="Birthday Cakes" description="homemade with love" />
+            <ProductCard image={Cupcakes} title="Cupcakes" description="homemade with love" />
+            <ProductCard image={img1} title="Swiss rolls" description="homemade with love" />
+            <ProductCard image={img1} title="Other desserts and bread" description="homemade with love" />
 
         </div>
     )
