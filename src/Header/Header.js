2aa2299078bad62f78cda9a41b2a7142b05e2a85
Jing Yang <jyang.ct@gmail.com>
2021-02-03 23:57:20 -0800

    add burger menu

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { ReactComponent as MenuIcon } from "../images/menu-24px.svg";
+import { ReactComponent as CloseMenu } from "../images/clear-24px.svg";
 
 function Header() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <div className="header">
       <div className="header__left">
         <h1 className="header__logo">The Taste</h1>
       </div>
-      <div className="header__center">
-
-        <h3> <Link className="link" to="/">Gallery</Link></h3>
-        <h3> <Link className="link" to="/order">Recipes</Link></h3>
-        <h3> <Link className="link" to="/about"> About</Link></h3>
+      <div className={click ? "nav-options active" : "nav-options"}>
+        <h3> <Link className="link" onClick={closeMobileMenu} to="/">Gallery</Link></h3>
+        <h3> <Link className="link" onClick={closeMobileMenu} to="/order">Recipes</Link></h3>
+        <h3> <Link className="link" onClick={closeMobileMenu} to="/about">About</Link></h3>
       </div>
-      <div className="header__right">
-        <p>social media</p>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+            <MenuIcon className="menu-icon" />
+          )}
       </div>
     </div>
   );
