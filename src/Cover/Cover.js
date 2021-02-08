3cd420f20408a36fe3ab99bbbaf011198ea2a5b4
Jing Yang <jyang.ct@gmail.com>
2021-02-07 23:51:15 -0800

    modify 3rd party library component style, override style

--- src/Cover/Cover.js
+++ src/Cover/Cover.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Cover.css';
 import ImageGallery from 'react-image-gallery';
-import a from '../images/carousel/a.JPG';
-import b from '../images/carousel/b.JPG';
+import a from '../images/carousel/a.jpg';
+import b from '../images/carousel/b.jpg';
 import c from '../images/carousel/c.JPG';
 import d from '../images/carousel/d.JPG';
 import e from '../images/carousel/e.JPG';
 import f from '../images/carousel/f.JPG';
 import thum_a from '../images/carousel/f-0.JPG';
-import thum_b from '../images/carousel/f-1.JPG';
+import thum_b from '../images/carousel/f-1.jpg';
 import thum_c from '../images/carousel/f-2.JPG';
 import thum_d from '../images/carousel/f-3.JPG';
 import thum_e from '../images/carousel/f-4.JPG';
@@ -20,8 +20,8 @@ function Cover() {
 
 
     return (
-        <div>
-            <ImageGallery showThumbnails="false" sizes="100px 100px" items={images} />
+        <div className="cover_div">
+            <ImageGallery showThumbnails={false} additionalClass="cover" items={images} />
         </div>
         /*  <div className="cover">
              <img src={coverimg} alt="cover" />
